3ad26cba0a44f1be53f8f29695cef35e146c120d
partybusiness <rrh@horriblepain.com>
2019-03-02 12:30:31 -0600

    Uploaded demon files

---
+++ Demon.shader
@@ -0,0 +1,89 @@
+﻿
+Shader "Unlit/Demon"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Columns ("Columns", int) = 8
+		_Rows("Rows", int) = 8
+		[PerRendererData]_Rotation ("Rotation", Range(0.0,1.0)) = 0
+		[PerRendererData]_Expression("Expression", Range(0.0,1.0)) = 0
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		Cull Back
+
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#pragma multi_compile_instancing
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float2 pos : TEXCOORD1;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed _Columns;
+			fixed _Rows;
+
+			UNITY_INSTANCING_CBUFFER_START(InstanceProperties)
+			UNITY_DEFINE_INSTANCED_PROP(fixed, _Rotation)
+			UNITY_DEFINE_INSTANCED_PROP(fixed, _Expression)
+			UNITY_INSTANCING_CBUFFER_END
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				UNITY_SETUP_INSTANCE_ID(v);
+				
+				// get the camera basis vectors
+				float3 forward = normalize(UNITY_MATRIX_V._m20_m21_m22);
+				float3 up = normalize(UNITY_MATRIX_V._m10_m11_m12);
+				float3 right = -normalize(UNITY_MATRIX_V._m00_m01_m02);
+
+				// rotation to face camera
+				float4x4 rotationMatrix = float4x4(right, 0,
+					up, 0,
+					forward, 0,
+					0, 0, 0, 1);
+
+				float3 viewDir = ObjSpaceViewDir(v.vertex);
+				float4 offset = float4(v.pos.x, v.pos.y, 0, 0);
+				v.vertex += mul(offset, rotationMatrix);
+				float viewRot = atan2(viewDir.z, viewDir.x) / (6.28318530718);
+
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.uv.x += round((UNITY_ACCESS_INSTANCED_PROP(_Rotation) + viewRot) * _Columns) / _Columns; //offset uv based on rotation
+				o.uv.y += round((UNITY_ACCESS_INSTANCED_PROP(_Expression)) * _Rows) / _Rows; //offset uv based on expression or animation
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				clip(col.a - 0.5);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ RenderDemons.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemonRender
+{
+    public class RenderDemons : MonoBehaviour
+    {
+
+        //what mesh should it render
+        [SerializeField]
+        private Mesh mesh;
+
+        //set the bounds for culling
+        [SerializeField]
+        private Bounds meshBounds = new Bounds(Vector3.zero, Vector3.one);
+
+        //what material should it render
+        [SerializeField]
+        private Material material;
+
+        //radius for random positions assigned to the demons
+        [SerializeField]
+        private float radius = 10f;
+
+        //how many demons
+        [SerializeField]
+        private int numDemons = 100;
+
+        [SerializeField]
+        private float rotationSpeed = 0.1f;
+
+        [SerializeField]
+        private float forwardSpeed = 5f;
+
+        private List<Matrix4x4> matrices;
+        private MaterialPropertyBlock properties;
+        private List<float> rotations;
+        private List<float> expressions;
+
+        void Start()
+        {
+            mesh.bounds = meshBounds;
+            matrices = new List<Matrix4x4>();
+            properties = new MaterialPropertyBlock();
+            rotations = new List<float>();
+            expressions = new List<float>();
+            for (int i = 0; i < numDemons; i++)
+            {
+                var posMat = new Matrix4x4();
+                posMat.SetTRS(transform.position + Random.insideUnitSphere * radius, Quaternion.identity, Vector3.one);
+                matrices.Add(posMat);
+                rotations.Add(Random.Range(0f, 1f));
+                expressions.Add(Random.Range(0f, 1f));
+            }
+            properties.SetFloatArray("_Rotation", rotations.ToArray());
+            properties.SetFloatArray("_Expression", expressions.ToArray());
+        }
+
+        void Update()
+        {
+            var tranMatrix = new Matrix4x4();
+            var moveVector = Vector3.forward * forwardSpeed * Time.deltaTime;
+            for (int i = 0; i < rotations.Count; i++)
+            {
+                rotations[i] += Time.deltaTime * rotationSpeed;
+                //tranMatrix.SetTRS(Quaternion.Euler(0,Mathf.Floor(rotations[i]*8f)*42.5f,0) * moveVector, Quaternion.identity, Vector3.one);
+                tranMatrix.SetTRS(Quaternion.Euler(0, Mathf.Floor(rotations[i] * 8f) * 45f, 0) * moveVector, Quaternion.identity, Vector3.one);
+                matrices[i] *= tranMatrix;
+            }
+            properties.SetFloatArray("_Rotation", rotations.ToArray());
+            Graphics.DrawMeshInstanced(mesh, 0, material, matrices, properties);
+        }
+    }
+
+}
